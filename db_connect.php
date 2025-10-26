a83bd95499a75ee4c27608f42f3582fd795773a1
JaoGregori <kubucho4@gmail.com>
2025-10-26 13:33:39 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $host = "db.fr-pari1.bengt.wasmernet.com";
 $user = "e12d0ed37cc080007a312aed9282";
 $pass = "068fe12d-0ed4-75b2-8000-a97de9edb56c";
 $db = "dbE6nbUDUQaDkyKvYHWMhGat";
-$port = "10272"
+$port = "10272";
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
@@ -12,4 +12,5 @@ if ($conn->connect_error) {
 }
 
 ?>
+
 
