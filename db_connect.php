bdddb424699fd5f29161692d5de23ced6016c7e2
JaoGregori <kubucho4@gmail.com>
2025-10-26 13:32:25 +0100

    Add port parameter to database connection

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,12 @@ $host = "db.fr-pari1.bengt.wasmernet.com";
 $user = "e12d0ed37cc080007a312aed9282";
 $pass = "068fe12d-0ed4-75b2-8000-a97de9edb56c";
 $db = "dbE6nbUDUQaDkyKvYHWMhGat";
+$port = "10272"
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
 
 ?>
+
