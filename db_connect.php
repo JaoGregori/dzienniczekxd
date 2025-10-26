623a5cf938ae2f9690cd9f9fea1e628830fe9e78
JaoGregori <kubucho4@gmail.com>
2025-10-26 13:26:58 +0100

    Update database connection details in db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,13 @@
 <?php
 // Połączenie z bazą danych
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "dziennik_szkolny";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$user = "e12d0ed37cc080007a312aed9282";
+$pass = "068fe12d-0ed4-75b2-8000-a97de9edb56c";
+$db = "dbE6nbUDUQaDkyKvYHWMhGat";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
+
 ?>
