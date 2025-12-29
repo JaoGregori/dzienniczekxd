b7516e14cee95cd61e958008915cc2aab8a8e354
JaoGregori <kubucho4@gmail.com>
2025-12-29 22:55:40 +0100

    Update database connection parameters

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,14 @@
 <?php
 // Połączenie z bazą danych
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "dziennik_szkolny";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$user = "2ecd8c60735880003a81557990ce";
+$pass = "06952ecd-8c60-79ba-8000-41090a05b758";
+$db = "dbN4zSt72x92xG3GvrYjYHXh";
+$port = "10272";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
+
 ?>
